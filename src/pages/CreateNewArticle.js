93cd7d9509b577cfc9e132d409b51a8570cf3010
prince0010 <princenagac12@gmail.com>
2023-03-16 00:56:42 +0800

    2

--- src/pages/CreateNewArticle.js
+++ src/pages/CreateNewArticle.js
@@ -5,7 +5,8 @@ return(
         <input type = 'title' placeholder={'Title'} />
         <input type ='summary' placeholder={'Summary'} />
         <input type ='file' />
-
+        {/* adding  a package called react-quill && the command is npm install react-quill --save */}
+        <textarea name ="" id="" cols = "30" rows= "10"></textarea>
         </form>
 
     </div>
