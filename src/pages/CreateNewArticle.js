a8e58e34e56f6fd01e12dbc1df2a364b804bc35d
prince0010 <princenagac12@gmail.com>
2023-03-17 01:00:59 +0800

    Create New App Added

--- src/pages/CreateNewArticle.js
+++ src/pages/CreateNewArticle.js
@@ -27,33 +27,45 @@ const modules = {
     const [files, setFiles] = useState('');
         
     async function createArticle (evt){
-      const data = new FormData();
-      data.set('title', title);
-      data.set('summary', summary);
-      data.set('content', content);
-      //Index in files will be 0 since it will only contain the first image or file that was being added even though there are many files will be inserted or added only the first file or image will be choosen
-      data.set('file', files[0]);
       evt.preventDefault();
-      // console.log(files);
-     const response = await fetch('http://localhost:4000/post', {
+      const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+
+        const response = await fetch('http://localhost:4000/post', {
         method: 'POST',
         body: data,
-      });
-      console.log(await response.json());      
+        });
+        console.log(await response.json());
     }
-
         return(
      <div className=' ml-16 pt-[100px]' >
         <form onSubmit={createArticle}>
-        <input type = 'title' placeholder={'Title'} value = {title} onChange = {evt => setTitle(evt.target.value)}/>
-        <input type ='summary' placeholder={'Suummary'} value = {summary} onChange = {evt => setSummary(evt.target.value)}/>
-        <input type ='file' onChange={evt => setFiles(evt.target.files)}/>
+        <input className='border border-black shadow-lg p-3 rounded-md  hover:outline-none hover:border-orange-300'
+         type = 'title'
+          placeholder={'Title'}
+           value = {title}
+            onChange = {evt => setTitle(evt.target.value)}
+            />
+        <input className='border border-black shadow-lg p-3 rounded-md hover:outline-none hover:border-orange-300'
+        type ='summary'
+         placeholder={'Summary'}
+          value = {summary}
+           onChange = {evt => setSummary(evt.target.value)}
+           />
+        <input className='border border-black shadow-lg p-3 rounded-md hover:outline-none hover:border-orange-300'
+        type ='file' 
+        onChange={evt => setFiles(evt.target.files)}
+        />
         {/* adding  a package called react-quill && the command is npm install react-quill --save */}
         <div className='ml-9 w-[72rem]'>
-        <ReactQuill value={content} modules ={modules} formats={formats} onChange ={newValue => setContent(newValue)}/>
+        <ReactQuill className='overflow-hidden'
+        value={content} modules ={modules} formats={formats} onChange ={newValue => setContent(newValue)}/>
         </div>
         <button className="m-auto w-[90%] grid items-center bord rounded-xl p-[6.8px] text-light bg-primaryblack transition
-              ease-in-out delay-150 hover:-translate-y-1 duration-300"> Create Post </button>
+              ease-in-out delay-150 hover:-translate-y-1 duration-300 hover:bg-orange-300 hover:text-black"> Create Post </button>
         </form>
 
     </div>
